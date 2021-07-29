33e086dd8778e4dd036d9d8b6fa4e4696f127e13
Litvinenko Boris <boris12lit@gmail.com>
2021-07-29 22:17:22 +0300

    added site

---
+++ js/main.js
@@ -0,0 +1,35 @@
+$( document ).ready(function() {
+
+
+
+  // Приветственная анимация
+  $( ".title " ).fadeOut(0);
+  $( ".title").delay(3200).fadeIn(2000);
+  $( ".shine" ).fadeOut(0);
+  $( ".shine" ).delay(1000).fadeIn(2500);
+  $( ".shine" ).delay(2500).fadeOut(1500);
+  
+  // $( ".title" ).delay(9200).addClass('shine');
+
+  $( ".main-video-text " ).fadeOut(0);
+  
+
+
+
+  $( ".is_overlay" ).addClass('js_overlay-on');
+
+  $( ".first-video-text ").delay(1200).fadeIn(1000);
+
+  $( ".first-video-text ").delay(400).fadeOut(1000);
+
+  $( ".main-video-text ").delay(3600).fadeIn(1000);
+
+  $( "#myBtn").delay(3600).fadeIn(1000);
+
+
+
+
+
+});
+
+
